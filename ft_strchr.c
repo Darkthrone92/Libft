3bb075ea31345cb6bd9bb5dad333b7d9d9d44a1b
root <root@DESKTOP-V9U3G6V>
2024-06-26 00:24:46 +0200

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,44 @@
 /*   By: xbatiste <xbatiste@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 20:31:44 by xbatiste          #+#    #+#             */
-/*   Updated: 2024/06/25 20:45:12 by xbatiste         ###   ########.fr       */
+/*   Updated: 2024/06/26 00:15:01 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strchr(const char *string, int c)
-{
+#include <stdio.h>
 
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
+			return (char *)&str[i];
+		}
+	i++;
+	}
+	if (c == '\0')
+		return (char *)&str[i];
+
+	return (NULL);
+}
+
+int	main(void)
+{
+	char *j = "Hola mundo";
+	char i = 'm';
+	
+	printf("Origen: %s\n", j);
+	printf("El carácter a buscar es: %c\n", i);
+
+	char *pos = ft_strchr(j, i);
+	
+	if(pos != NULL)
+		printf("La posición es: %ld\n", pos - j);
+	else
+		printf("EL carácter no se ha encontrado.\n");
+	return (0);
 }
