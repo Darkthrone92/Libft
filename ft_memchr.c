3c9e75c39fa8bd1598fc9ffd3e90d8c776e9dd56
Xavier Batiste <xbatiste@car11s5.42barcelona.com>
2024-06-26 21:34:17 +0200

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xbatiste <xbatiste@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 20:43:50 by xbatiste          #+#    #+#             */
+/*   Updated: 2024/06/26 21:30:59 by xbatiste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*src;
+	unsigned char		value;
+	size_t				i;
+
+	src = (const unsigned char *)s;
+	value = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (src[i] == value)
+		{
+			return ((void *)&src[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	char str[] = "Lorem ipsum dolor sit amet";
+	char val = 'd';
+	size_t bytes = 26;
+
+	char *result = ft_memchr(str, val, bytes);
+
+	if (result != NULL)
+		printf("El valor %c está en pos: %ld\n", val, result - str);
+	else
+		printf("El valor %c no se encontró en %zu bytes\n", val, bytes);
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xbatiste <xbatiste@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:21:27 by xbatiste          #+#    #+#             */
-/*   Updated: 2024/06/26 20:08:03 by xbatiste         ###   ########.fr       */
+/*   Updated: 2024/06/26 20:45:32 by xbatiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_isascii(int c);
 int	ft_isalnum(int c);
 int	ft_isprint(int c);
 int	ft_toupper(int c);
-int	strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -36,6 +36,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n)
 void	ft_bzero(void *s, size_t n);
 
 #endif
