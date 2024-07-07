fc28ae4a445cb8231acddb31ba0f06b3cc2e71de
Xavier <xavierbatiste92@gmail.com>
2024-07-07 19:52:52 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*result;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	result = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+/*int	main(void)
+{
+	char	*s = "Hola mundo!";
+	char	*res;
+
+	res = ft_strmapi(s, name_function);
+	if (result == NULL)
+	{
+		printf("MEmory allocation failed.\n");
+		return (1);
+	}
+	printf("Original string: %s.\n", s);
+	printf("Resulting string: %s.\n", res);
+	free(res);
+	return (0);
+}*/
